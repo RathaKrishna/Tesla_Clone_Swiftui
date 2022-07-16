acd7ca163f1cbf289f311a727094c0cbdbd2f5ef
Rathakrishnan <born4winkrishna@gmail.com>
2022-07-16 14:43:25 +0800

    Initial Commit

---
+++ TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by Rathakrishnan Ramasamy on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
