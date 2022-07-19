fb1cc88c5c8a99d69855fceebbec34a00c7a6a83
Rathakrishnan <born4winkrishna@gmail.com>
2022-07-19 14:38:35 +0800

    Update

---
+++ TeslaApp/Components/ControlsComponents.swift
@@ -0,0 +1,22 @@
+//
+//  ControlsComponents.swift
+//  TeslaApp
+//
+//  Created by Rathakrishnan Ramasamy on 16/07/22.
+//
+
+import SwiftUI
+
+struct IconButton: View {
+    var icon: String
+    var text: String
+    var body: some View {
+        Button(action: {}){
+            Label(text, systemImage: icon)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 44)
+        .background(Color.white.opacity(0.09))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
